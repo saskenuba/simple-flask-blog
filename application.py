cce7a5a3f61b17ad182fc0b3014ad949904b19f1
Martin Mariano <martin@hotmail.com.br>
2017-07-24 04:57:21 -0300

    using semantic ui now

--- application.py
+++ application.py
@@ -19,3 +19,15 @@ def index():
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+
+# dashboard for logged in user
+@app.route('/<username>')
+def dashboard(username):
+    return render_template("dashboard.html")
+
+
+# dashboard for logged in user
+@app.route('/newpost')
+def newpost():
+    return render_template("newpost.html")
