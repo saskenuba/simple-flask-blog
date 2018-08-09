156b4b4c3efbc232b37fa0ccadb445c53f754027
Martin <martin@hotmail.com.br>
2018-08-09 06:09:05 -0300

    added info on setup

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup
 setup(
     name='martinblog',
     packages=['martinblog'],
+    version='0.4.9',
+    author='Martin Mariano',
+    author_email='contato@martinmariano.com',
     include_package_data=True,
     install_requires=[
-        'flask', 'jinja2', 'flask-sqlalchemy', 'flask-jsglue', 'flask-login', 'wtforms',
-        'wtforms-json', 'requests', 'python-slugify', 'bcrypt', 'flask-migrate', 'flask-restplus'
+        'flask', 'jinja2', 'flask-sqlalchemy', 'flask-jsglue', 'flask-login',
+        'wtforms', 'wtforms-json', 'requests', 'python-slugify', 'bcrypt',
+        'flask-migrate', 'flask-restplus'
     ],
     extra_require={
         'pg': ['psycopg2'],
         'deploy': ['gunicorn']
-    }
-)
+    })
